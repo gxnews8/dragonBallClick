df0ac6e6e2932afbab5633f36b87c7585e134623
Davis Herbig <davis.herbig@gmail.com>
2017-02-15 12:14:40 -0800

    Fleshed out Trainer / Player interaction, Added PowerUp, AddTrainer, and Trainer Creation methods.

--- Player.cs
+++ Player.cs
@@ -3,9 +3,28 @@ namespace dragonBallClick {
     public class Player{
         public string Name;
         public string Password;
-        public int lifePower;
+        public int TotalPower;
         public int Power;
         public int Level;
+        public int PPS;
         public List<Trainer> Trainers;
+
+        public Player(string Alias, string shibboleth){
+            this.Name = Alias;
+            this.Password = shibboleth;
+            Power = 0;
+            Level = 1;
+            PPS = 0;
+        }
+        public AddTrainer(string Name){
+            Trainer theTrainer = new Trainer(Name);
+            PPS += theTrainer.ClickSpeed;
+            Trainers.Add(theTrainer);
+        }
+        public PowerUp(){
+            Power += Level;
+            TotalPower += Level;
+        }
     }
+
 }
